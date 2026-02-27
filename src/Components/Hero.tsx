a0ab976d33fcbc579b38008fab648922271df6b0
Harshvardhan-21 <harshvardhanraj332@gmail.com>
2026-02-27 16:04:08 +0530

    new

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,7 +2,8 @@ const Hero = () => {
   return (
     <section className="hero">
       <h1>Hi, I'm Harshvardhan</h1>
-      <p>I am Currently Persuing BCA from Guru Kashi University.I am also working in SRV Electricals as a IT Developer</p>
+      <p>I am Currently Persuing BCA from Guru Kashi University.</p>
+      <p>I am also working in SRV Electricals as a IT Developer.</p>
       <p>Frontend Developer | React | TypeScript</p>
       <p> I had also does Internship from CodTech IT solution in Python Programming</p>
       <a href="#projects" className="btn">View My Work</a>
